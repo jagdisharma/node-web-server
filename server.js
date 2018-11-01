ed69f140ba9d23b64caec8e5a4b7ddfb0f9be29a
Jagdish Sharma <jagdishcsharma29@gmail.com>
2018-11-01 18:29:19 +0530

    Projects page setup

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/projects',(req, res) => {
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  })
+});
+
 app.listen(port, () => {
   console.log(`Listening to port no ${port}`);
 });
